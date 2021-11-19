54e8f345fa5cb9515934b8a5f305b0261204c6e0
romain plantey <plantey.romain@gmail.com>
2021-11-19 12:21:29 +0100

    new seed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,18 @@ require "rest-client"
 # Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "let's delete database"
+Movie.destroy_all
+puts "DB destroyed"
 
-
+puts "let's launch the parsing"
 
 response = RestClient.get "http://tmdb.lewagon.com/movie/top_rated"
 payload = JSON.parse(response)
 
-payload["results"].first(15).each do |result|
+puts "let's add new movies"
+
+payload["results"].last(5).each do |result|
   Movie.create!(
     title: result["title"],
     overview: result["overview"],
@@ -20,3 +25,5 @@ payload["results"].first(15).each do |result|
     rating: result["vote_average"],
   )
 end
+
+puts "movies added "
